c5bebcfff056f61da99ec88e55d0345fe781238b
Maja Gedda <maja.gedda@zocom.se>
2020-10-29 11:55:02 +0100

    init repo

---
+++ script.js
@@ -0,0 +1,103 @@
+fighters = [
+    {
+        id: 0,
+        name: 'Smart and ripped monkey',
+        speciality: 'rip your face off',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+
+        }
+    },
+    {
+        id: 1,
+        name: 'Zombie snake',
+        speciality: 'bite',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+            
+        }
+    },
+    {
+        id: 2,
+        name: 'Sociopathic ghost',
+        speciality: 'manipulate',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+            
+        }
+    },
+    {
+        id: 3,
+        name: 'A fast Frankenstein',
+        speciality: 'like regular Frankenstein, but fast',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+            
+        }
+    },
+    {
+        id: 4,
+        name: 'Count Dracula',
+        speciality: 'sleep, spook, drink blood, repeat',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+            
+        }
+    },
+    {
+        id: 5,
+        name: 'Your crazy nazi aunt',
+        speciality: 'make people uncomfortable',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+            
+        }
+    },
+    {
+        id: 6,
+        name: 'A kid high on candy',
+        speciality: 'give headaches',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+            
+        }
+    },
+    {
+        id: 7,
+        name: 'That girl from Grudge',
+        speciality: 'spook',
+        addFighter: function() {
+            //Write a function that adds your fighter to your list 
+            //both in innerHTML and the empty list here
+            
+        }
+    },
+]
+
+let yourFighters = [];
+
+//Del 1
+//Lägg till eventlyssnare på fighter-knapparna
+//Kalla på dess fighters objekt-metod
+
+
+
+//Del 2
+//Skriv objekt-metoderna
+
+//Lägg in vald fighter i listan 'yourFighters'
+//Lägg in namnet i en <li> i HTML-en under 'Your Fighters'
+
+
+
+
+//Some next level shit
+//När du klickat på ett val ur den vänstra listan, 
+//så ska det objektet försvinna ur den listan och från HTML'en
